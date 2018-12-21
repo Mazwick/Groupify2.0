5bc4ea27c225e8004730addeefa271e6c7b612a6
Corey Gallagher <coreygallagher@coreys-macbook-pro.local>
2018-12-21 10:42:07 -0600

    Initial Commit

---
+++ Groupify2.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Groupify2.0
+//
+//  Created by Corey Gallagher on 12/21/18.
+//  Copyright © 2018 district196.org. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
